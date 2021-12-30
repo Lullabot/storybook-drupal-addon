85d62ae8485a1f8041f0d28c37b0b75ee48080cb
Mateu Aguiló Bosch (e0ipso) <mateu@mateuaguilo.com>
2021-12-30 16:32:46 +0100

    fix: remove unnecessary key

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
 export const parameters = {
   drupalTheme: 'lorem',
   supportedDrupalThemes: {
-    lorem: {title: 'Lorem Theme', machineName: 'lorem'},
-    ipsum: {title: 'Ipsum', machineName: 'ipsum'},
+    lorem: {title: 'Lorem Theme'},
+    ipsum: {title: 'Ipsum'},
   }
 };
