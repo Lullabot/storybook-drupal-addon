00c44ad1c5a4172f659bb51c75b91bc260697896
Mateu Aguiló Bosch (e0ipso) <mateu@mateuaguilo.com>
2022-02-28 08:17:06 +0100

    fix: improve re-assignment

--- src/fetchStoryHtml.ts
+++ src/fetchStoryHtml.ts
@@ -19,9 +19,12 @@ function createNewBody(htmlDoc: Document): Node {
   const newBody = htmlDoc.createElement('body');
   // Copy the body attributes from the old body to the new, in case there is
   // anything functionally relevant.
-  htmlDoc.body.getAttributeNames()
-    .map(htmlDoc.body.getAttributeNode.bind(htmlDoc.body))
-    .forEach(newBody.setAttributeNode.bind(newBody));
+  const bodyAttrs = htmlDoc.body.getAttributeNames()
+    .map<Attr|null>(htmlDoc.body.getAttributeNode.bind(htmlDoc.body))
+    .filter(Boolean);
+  for (const bodyAttr of bodyAttrs) {
+    newBody.setAttributeNode(bodyAttr);
+  }
   newBody.innerHTML = clWrapper.innerHTML;
   // Include the Drupal "js footer" assets, i.e., all the <script> tags in
   // the <body>.
