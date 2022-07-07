7df1b33f4d1840b307a104cfe17c1f8666979285
Mateu Aguiló Bosch (e0ipso) <mateu@mateuaguilo.com>
2022-07-07 23:23:42 +0200

    chore: update lint suggestions

--- src/Tool.tsx
+++ src/Tool.tsx
@@ -1,28 +1,24 @@
-import React, {ReactElement} from 'react';
-import {useGlobals} from '@storybook/api';
+import React, { ReactElement } from 'react';
+import { useGlobals } from '@storybook/api';
 import {
   IconButton,
-  Link,
+  Link as LinkStorybook,
   TooltipLinkList,
-  WithTooltip
+  WithTooltip,
 } from '@storybook/components';
-import {TOOL_ID} from './constants';
+import { TOOL_ID } from './constants';
 
-export interface Link {
-  id: string;
+export interface Link extends LinkStorybook {
   active: boolean;
-  onClick: () => void;
   title: string;
 }
 
-type ExpandedThemeValue = { title: string; };
-export type ThemeValue =
-  | string
-  | ExpandedThemeValue;
+type ExpandedThemeValue = { title: string };
+export type ThemeValue = string | ExpandedThemeValue;
 
 const getValue = (value: ThemeValue): ExpandedThemeValue => {
   if (typeof value === 'string') {
-    return {title: value};
+    return { title: value };
   }
   return {
     title: value.title || '',
@@ -32,46 +28,47 @@ const getValue = (value: ThemeValue): ExpandedThemeValue => {
 const getDrupalThemes = (
   themes: Record<string, ThemeValue>,
   theme: string,
-  onSelect: (selected: string) => void
+  onSelect: (selected: string) => void,
 ): Link[] =>
   themes
     ? Object.entries(themes).map(([key, value]) => ({
-      ...getValue(value),
-      id: key,
-      active: key === theme,
-      onClick: () => onSelect(key)
-    }))
+        ...getValue(value),
+        id: key,
+        active: key === theme,
+        onClick: () => onSelect(key),
+      }))
     : [
-      {
-        id: 'none',
-        title: 'No themes in parameters',
-        active: true,
-        onClick: () => {
+        {
+          id: 'none',
+          title: 'No themes in parameters',
+          active: true,
+          onClick: () => {},
         },
-      }
-    ]
+      ];
 
 export const Tool = (): ReactElement => {
-  const [{drupalTheme, supportedDrupalThemes}, updateGlobals] = useGlobals();
+  const [{ drupalTheme, supportedDrupalThemes }, updateGlobals] = useGlobals();
 
   return (
     <WithTooltip
       closeOnClick={true}
       placement="top"
       trigger="click"
-      tooltip={({onHide}) => (
+      tooltip={({ onHide }) => (
         <TooltipLinkList
           links={getDrupalThemes(
             supportedDrupalThemes,
             drupalTheme,
             (selected) => {
               if (selected !== drupalTheme) {
-                updateGlobals({drupalTheme: selected});
+                updateGlobals({ drupalTheme: selected });
               }
               onHide();
-            })}
+            },
+          )}
         />
-      )}>
+      )}
+    >
       <IconButton
         key={TOOL_ID}
         active={drupalTheme}
@@ -79,7 +76,6 @@ export const Tool = (): ReactElement => {
       >
         💧 Drupal Theme
       </IconButton>
-
     </WithTooltip>
   );
 };
