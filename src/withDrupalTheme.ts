6b0b497c6003931b3d67c60f59a6d3e8da9574ba
Mateu Aguiló Bosch (e0ipso) <mateu@mateuaguilo.com>
2022-09-16 13:26:39 +0200

    Revert "fix: what is even the point"
    This reverts commit af5219be6e73f7a956980c4f90c07bbf5839cc11.

--- src/withDrupalTheme.ts
+++ src/withDrupalTheme.ts
@@ -2,15 +2,31 @@ const globalWindow = require('global/window');
 import {
   StoryContext,
   StoryFn as StoryFunction,
+  useCallback,
   useEffect,
   useGlobals,
+  useState,
 } from '@storybook/addons';
+
+const defaultDebounceTimeout = 300;
+const heartBeatEmoji = '\uD83D\uDC93';
 
 export const withDrupalTheme = (
   StoryFn: StoryFunction,
   context: StoryContext,
 ) => {
   const [globals, updateGlobals] = useGlobals();
+  const refresh = useCallback(() => {
+    const delay = globals?.debounceTimeout || defaultDebounceTimeout;
+    const isLocked = globals?.debounceTimeout || false;
+    if (!isLocked) {
+      // Lock the refresh procedure.
+      updateGlobals({isLocked: true})
+      globalWindow.document.location.reload();
+      // Unlock it again soon.
+      setTimeout(() => updateGlobals({isLocked: false}), delay);
+    }
+  }, [globals]);
   useEffect(() => {
     const {
       parameters: {drupalTheme, supportedDrupalThemes},
@@ -21,6 +37,38 @@ export const withDrupalTheme = (
       } else {
         updateGlobals({supportedDrupalThemes});
       }
+    }
+  }, [globals]);
+
+  useEffect(() => {
+    const hmr = globalWindow?.__whmEventSourceWrapper?.['/__webpack_hmr'];
+    if (!hmr) {
+      return;
+    }
+    hmr.addMessageListener(handleMessage);
+
+    function handleMessage(event: MessageEvent) {
+      if (event.data == heartBeatEmoji) {
+        return;
+      }
+      let data;
+      try {
+        data = JSON.parse(event.data);
+      } catch (ex) {
+        console.warn('Invalid HMR message: ' + event.data + '\n' + ex);
+        return;
+      }
+      const currentHash = globals?.hash;
+      const newHash = data?.hash;
+      if (!newHash) {
+        return;
+      }
+      currentHash === newHash
+        // If nothing changed in the Webpack hash, it may mean changes in the
+        // server components.
+        ? refresh()
+        // Store the hash in the globals because state will reset every time.
+        : updateGlobals({hash: newHash});
     }
   }, [globals]);
 
