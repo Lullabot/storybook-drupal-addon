5c2b85d5a29f1318c804b4f66d736aa9dbd230ce
Andrew Berry <andrew.berry@lullabot.com>
2023-03-01 12:22:05 -0500

    fix: loading storybook/drupal-addon twice

--- preset.js
+++ preset.js
@@ -2,11 +2,6 @@ function config(entry = []) {
   return [...entry, require.resolve('./dist/esm/preset/preview')];
 }
 
-function managerEntries(entry = []) {
-  return [...entry, require.resolve('./dist/esm/preset/manager')];
-}
-
 module.exports = {
-  managerEntries,
   config,
 };
