85cc448c8419a57313b297e819757afad5df5e99
Theodoros Ploumis <me@theodorosploumis.com>
2023-06-08 15:28:19 +0300

    Move the suggested localDev parameter into preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,7 @@ import type {Preview} from '@storybook/react';
 
 const preview: Preview = {
   globals: {
+    localDev: false,
     drupalTheme: 'umami',
     supportedDrupalThemes: {
       umami: {title: 'Umami'},
