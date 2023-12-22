58e493ec5fb46d65ec06e8c8250b23992957890a
Mateu Aguiló Bosch (e0ipso) <mateu@mateuaguilo.com>
2023-12-22 10:59:38 +0100

    feat: use yarn berry (v4)
    For now we are using the node-modules linker, as there is a problem with pnp

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,15 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "./local-preset.js",
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("./local-preset.js"),
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
   docs: {
@@ -16,3 +17,7 @@ const config: StorybookConfig = {
   },
 };
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
